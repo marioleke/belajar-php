797fd13265d1215731034950998059db704bc71b
marioleke <erpinleke@gmail.com>
2022-10-26 23:32:29 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!!!<br>";
+
+    $nama = "Mario";
+    $usia = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia <strong>$usia</strong> tahun.<br>";
+
+    $namaAdik = "Fidelio";
+    $usiaAdik = 6;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia <strong>$usiaAdik</strong> tahun.<br>";
+    
+    $selisihUmur = $usia - $usiaAdik;
+    echo "Selisih usia saya dan adik saya adalah <strong>$selisihUmur</strong> tahun."
+
+?>
